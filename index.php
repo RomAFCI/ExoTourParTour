43a11ceb057390171af3d84c220551a712a71613
RomAFCI <adnot.rom@gmail.com>
2025-07-29 11:10:27 +0200

    save

--- index.php
+++ index.php
@@ -1,4 +1,71 @@
 <?php
 
+// class parent
+class Perso
+{
 
-?>
+    public $nom;
+    public $pv;
+    public $atk;
+
+    // Constructor
+    public function __construct($nom, $pv, $atk)
+    {
+        $this->nom = $nom;
+        $this->pv = $pv;
+        $this->atk = $atk;
+    }
+
+    // Getter
+
+    // Setter
+
+    // Method
+
+    public function attaquer(Perso $adversaire) {}
+
+    public function recevoirDegats(int $degats) {}
+
+    public function afficherEtat() {}
+}
+
+// class enfants
+class Guerrier extends Perso {
+
+public function __construct($nom, $pv, $atk) {
+    $nom->nom = $nom;
+    $pv->pv = "120";
+    $atk->atk = "15";
+
+// Particularité : dégâts constants, bonne résistance.
+
+}
+}
+
+class Voleur extends Perso {
+
+public function __construct($nom, $pv, $atk) {
+    $nom->nom = $nom;
+    $pv->pv = "100";
+    $atk->atk = "12";
+
+// Particularité : a une chance (30%) d’esquiver une attaque (aucun dégât subi).
+
+}
+}
+
+class Magicien extends Perso {
+
+public function __construct($nom, $pv, $atk) {
+    $nom->nom = $nom;
+    $pv->pv = "90";
+    $atk->atk = "8";
+
+// Particularité : a une chance (50%) de lancer un sort spécial qui double les dégâts infligés.
+
+}
+}
+
+$warrior1 = new Guerrier('Barbarius', 120, 15);
+
+var_dump($warrior1);
---
+++ note.php
@@ -0,0 +1,18 @@
+<?php
+
+// 1.Le joueur choisit un personnage parmi Guerrier, Voleur ou Magicien
+// (en dur dans le code pour simplifier).
+
+// 2.L’adversaire est généré automatiquement avec une autre classe.
+
+// 3.À chaque tour :
+// Le joueur attaque l’adversaire.
+// Puis l’adversaire attaque le joueur.
+// Affichez les points de vie restants.
+
+// 4.Le jeu continue jusqu’à ce qu’un des deux ait vie <= 0.
+
+// 5.Affichez le nom du gagnant.
+
+
+?>
